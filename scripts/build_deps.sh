de13d9d08b5474ce871fb3769ddc41d043aea215
袁 再权 <evils.ynk@gmail.com>
2024-06-04 15:57:44 +0800

    b

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,5 +1,22 @@
 #!/bin/sh -e
-
+$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --arch=arm64 --install-dir=./toolchain
+export ANDROID_HOME=`pwd`
+export TOOLCHAIN=$ANDROID_HOME/toolchain
+export CROSS_SYSROOT=$TOOLCHAIN/sysroot
+export PATH=$TOOLCHAIN/bin:$PATH
+export TOOL=arm-linux-androideabi
+export CC=$TOOLCHAIN/bin/${TOOL}-gcc
+export CXX=$TOOLCHAIN/bin/${TOOL}-g++
+export LINK=${CXX}
+export LD=$TOOLCHAIN/bin/${TOOL}-ld
+export AR=$TOOLCHAIN/bin/${TOOL}-ar
+export RANLIB=$TOOLCHAIN/bin/${TOOL}-ranlib
+export STRIP=$TOOLCHAIN/bin/${TOOL}-strip
+export ARCH_FLAGS="-mthumb"
+export ARCH_LINK=
+export CFLAGS="${ARCH_FLAGS} -fPIE -fPIC -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64"
+export CXXFLAGS="${CFLAGS} -frtti -fexceptions"
+export LDFLAGS="${ARCH_LINK} -pie"
 ./build.uv.sh
 ./build.hwloc.sh
 ./build.openssl3.sh
